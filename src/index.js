6c8e57f1be9132bd376c8bd0721158d3991ed40b
codergirlshecodes <acacchiola180@gmail.com>
2024-01-14 18:05:49 -0500

    added in Javscript

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-<script>
-        alert("Hello World");
-    </script>
+
+let typewriter = new Typewriter("#poem", {
+    strings: "Ma Il Cuore Non Ascolti Le Ragioni",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+});
+
+
+let instructionsElement = document.querySelector(".instructions");
+
+instructionsElement.addEventListener("focus", () => {
+    typewriter.stop();
+});
+
+instructionsElement.addEventListener("blur", () => {
+    typewriter.start();
+});
+function generatePoem(event) {
+    event.preventDefault();
+
+    let instructionsElement = document.querySelector(".instructions");
+    let poemElement = document.querySelector("#poem");
+
+    
+    let userInstructions = instructionsElement.value;
+    poemElement.innerHTML = userInstructions;
+
+   
+    typewriter.start();
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
