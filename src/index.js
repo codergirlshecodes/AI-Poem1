6d33349cb7afd54f6307d904584304656a68ca70
codergirlshecodes <acacchiola180@gmail.com>
2024-01-14 19:24:15 -0500

    added api key and adjusted html, javascript and css

--- src/index.js
+++ src/index.js
@@ -1,34 +1,43 @@
+let typewriter; 
 
-let typewriter = new Typewriter("#poem", {
-    strings: "Ma Il Cuore Non Ascolti Le Ragioni",
-    autoStart: true,
-    delay: 1,
-    cursor: "",
-});
+function displayPoem(response) {
+    console.log("poem generated");
 
+    let poemElement = document.querySelector("#poem");
+    poemElement.innerHTML = response.data.answer;
+}
 
-let instructionsElement = document.querySelector(".instructions");
-
-instructionsElement.addEventListener("focus", () => {
-    typewriter.stop();
-});
-
-instructionsElement.addEventListener("blur", () => {
-    typewriter.start();
-});
 function generatePoem(event) {
     event.preventDefault();
 
-    let instructionsElement = document.querySelector(".instructions");
-    let poemElement = document.querySelector("#poem");
-
-    
+    let instructionsElement = document.querySelector("#user-instructions");
     let userInstructions = instructionsElement.value;
-    poemElement.innerHTML = userInstructions;
 
-   
-    typewriter.start();
+    let apikey = "1at61ad039d332fc00c4boee9e4a24c4";
+    let prompt = `Generate a poem in any language ${userInstructions}`;
+    let context = "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML. If they only write topic make poem in English only. Make sure to follow user instructions";
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
+
+    console.log("Generating poem");
+    console.log(`Prompt: ${prompt}`);
+    console.log(`Context: ${context}`);
+
+    axios.get(apiURL).then(displayPoem);
 }
+
+let instructionsElement = document.querySelector("#user-instructions");
+
+instructionsElement.addEventListener("focus", () => {
+    if (typewriter) {
+        typewriter.stop();
+    }
+});
+
+instructionsElement.addEventListener("blur", () => {
+    if (typewriter) {
+        typewriter.start();
+    }
+});
 
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
